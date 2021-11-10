c695fa9e939f0c8196bc52a16ce5f8efec42e32f
yuvraj202 <78161417+yuvraj202@users.noreply.github.com>
2021-11-10 16:45:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function btn1()
+{
+    window.location=("index1.html");
+}
+
+function btn2()
+{
+    window.location=("index2.html");
+}
+
+function btn3()
+{
+    window.location=("index3.html");
+}
